898be088dfa3286fcde171abbbfd9225605926ab
Dat <nguyendinhdat343@gmail.com>
2026-01-30 15:19:46 +0700

    adjust import

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-import { useState } from 'react'
-import reactLogo from '/assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { Hero } from './components/Hero'
 import { About } from './components/About'
 import { Skills } from './components/Skills'
 import { Projects } from './components/Projects'
 import { Contact } from './components/Contact'
-import { Footer } from './components/Footer'
 import { Header } from './components/Header'
 
 function App() {
