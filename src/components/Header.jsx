e2d53c0eda9ce3d408e857a812b6df2b3a094ed2
Dat <nguyendinhdat343@gmail.com>
2026-02-05 00:20:53 +0700

    smooth behavior

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -25,7 +25,7 @@ export function Header() {
       <div className="max-w-6xl mx-auto px-4">
         <div className="flex items-center justify-between h-16">
             
-          <a href="#" className="text-xl">
+          <a href='#' className="text-xl">
             Nguyen Dinh Dat
           </a>
 
